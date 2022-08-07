15df54bb99c0ba43cb58d8e7c559de40524b9a89
Jean Dos Santos <jeandsantos88@gmail.com>
2022-08-07 12:30:33 +0200

    added change_column_types to Data class

--- processing/Data.py
+++ processing/Data.py
@@ -1,114 +1,116 @@
-from collections import defaultdict
-import numpy as np
-import pandas as pd
 
-class Recipe:
-    """A class to process hierarchical formulation data
+import re
+
+import pandas as pd
+import numpy as np
+class Data:
+    """
+    A class to process data 
     """
 
-    def __init__(self, df: pd.DataFrame, params: dict = None, exclude_highest_level = "auto", name: str = None):
+    def __init__(self, df: pd.DataFrame, verbose: bool = True):
         
-        self.df_ = df
-        self.df_.columns = [x.strip() for x in self.df_.columns]
-        
-        if params is None:
-            self.params = {"ing": "Component", "qty": "Qty", "lvl": "Level"}
+        if type(df) is not pd.DataFrame:
+            raise TypeError("Wrong data type, a pandas.DataFrame is expected")
         else:
-            self.params = params
+            self._df = df
+        
+        self.verbose = verbose
+        
+        self._update_columns()
+        self._update_column_types()
+        self._update_dimensions()
 
-        self.ing_key, self.qty_key, self.lvl_key = list(self.params.keys())[:3]
-        self.ing_col, self.qty_col, self.lvl_col = list(self.params.values())[:3]
+
+    def change_column_types(self, dtype_dict:dict):
+        self._df.astype(dtype=dtype_dict)
+        self._update_column_types()
         
-        if exclude_highest_level == "auto":
-            pass
-            
-        self.highest_level = self.df_[self.lvl_col].min()
-        self.recipe_ = self.df_[self.df_[self.lvl_col] > 1].reset_index(drop=True)
-       
-        if name is None:
-            self.name = df.loc[df[self.lvl_col] == self.highest_level, self.ing_col][0]
-        else:
-            self.name = name
+        if self.verbose:
+            for col, type in dtype_dict.items():
+                print(f'Changed \'{col}\' to \'{type}\' type')
+
         
-        self.rec_dict = defaultdict(self.dict_template)
-   
-    def dict_template(self):
-       return {key: [] for key in self.params.keys()}
-   
-    def copy_to_dict(self, idx, level):
-        for key, colname in self.params.items():
-            self.rec_dict[level][key].append(self.recipe_.loc[idx][colname])
-    
+    def remove_constant_columns(self):
+        constant_bool = self._df.apply(lambda x: len(x.unique()) == 1)
+        constant_cols = self._df.columns[constant_bool]
+
+        self._df = self._df.drop(columns=constant_cols)        
+        
+        if self.verbose:
+            print(f'Found {np.sum(constant_bool)} constant column(s). Removing columns:\n{constant_cols.tolist()}')
+
+
+    def print_column_types(self):
+        print(f'There are {len(self._cat_columns)} categorical fields:\n{self._cat_columns}')
+        print(f'There are {len(self._num_columns)} numerical fields:\n{self._num_columns}')
+
+
+    def _update_column_types(self):
+        
+        self._cat_columns = self._df.select_dtypes(include=['O']).columns.to_list()
+        self._num_columns = self._df.select_dtypes(include=['float64', 'int64']).columns.to_list()
+
+        
+    def _update_dimensions(self):
+        self._nrows, self._ncols  = self._df.shape
+
+        
+    def _update_columns(self):
+        self._columns = self._df.columns
+
+        
     def __repr__(self):
-        return f"Recipe(df)"
+        return f"Data(df)"
     
     def __str__(self):
-        n_components = self.recipe_.shape[0]
-        return "{} recipe with {} components".format(self.name, n_components)
-    
-    def traverse_recipe(self, idx = 0, start_level = 1, max_level = np.inf):
-
-        current_level = self.recipe_.loc[idx, self.lvl_col]
-            
-        if (idx == self.recipe_.index.max()):
-            previous_level = self.recipe_.loc[idx-1, self.lvl_col]
-            
-            if (current_level <= max_level) and (previous_level >= current_level):
-                self.copy_to_dict(idx, level = max_level)
-                return None # default_dict
-            
-            elif (idx == self.recipe_.index.max()) and (current_level > max_level):
-                return None # default_dict
-            
-        else:
-            next_level = self.recipe_.loc[(idx+1), self.lvl_col]
-            
-            if ~(next_level <= current_level) and ~(current_level > max_level) and (next_level > max_level):
-                self.copy_to_dict(idx, level = max_level)
-                self.traverse_recipe(idx + 1, start_level, max_level)
-                
-            elif (next_level <= current_level) and ~(current_level > max_level) and ~(next_level > max_level):
-                self.copy_to_dict(idx, level = max_level)
-                self.traverse_recipe(idx + 1, start_level, max_level)
-                
-            else:
-                self.traverse_recipe(idx + 1, start_level, max_level)
-
-    def extract_recipe(self, max_level = np.inf, aggregate = False, aggregate_by = None, sort = True, sort_by = None, ascending = False):
-
-        if max_level in self.rec_dict.keys():
-            res = pd.DataFrame(self.rec_dict[max_level])
-        else:
-            self.traverse_recipe(idx = 0, max_level = max_level)
-            res = pd.DataFrame(self.rec_dict[max_level])
-            
-        if aggregate:
-            if aggregate_by is None:
-                aggregate_by = self.ing_key
-
-            res = res.groupby(aggregate_by).sum().reset_index()[[self.ing_key, self.qty_key]]
-            if sort:
-                if sort_by is None:
-                    sort_by = self.qty_key
-        
-                res = res.sort_values(sort_by, ascending = ascending).reset_index(drop=True)
-
-        return res
+        return "A dataframe with {} rows and {} columns".format(self._nrows, self._ncols)
     
     @property
     def df(self):
-        return self.df_
+        return self._df
+
+    @property
+    def columns(self):
+        return self._columns
+
+    @property
+    def ncols(self):
+        return self._ncols
     
     @property
-    def recipe(self):
-        return self.recipe_
+    def num_columns(self):
+        return self._num_columns
     
+    @property
+    def cat_columns(self):
+        return self._cat_columns
+    
+    @property
+    def nrows(self):
+        return self._nrows
+
+    @df.setter
+    def df(self, new_df):
+        if type(new_df) is not pd.DataFrame:
+            raise TypeError("Wrong data type, a pandas.DataFrame is expected")
+        else:
+            self._df = new_df
+            self._columns = self._df.columns
+
+    @columns.setter
+    def columns(self, new_columns):
+        self._df.columns = new_columns
+        self._update_columns()
+        self._update_column_types()
+
+    @cat_columns.setter
+    def cat_columns(self, new_columns):
+        self._cat_columns = new_columns
+        self._update_columns()
+        self._update_column_types()
+            
     @classmethod
     def from_csv(cls, filepath: str, *args, **kwargs):
         df = pd.read_csv(filepath, *args, **kwargs)
-        return cls(df)
-
-    @classmethod
-    def from_excel(cls, filepath: str, *args, **kwargs):
-        df = pd.read_excel(filepath, *args, **kwargs)
         return cls(df)
